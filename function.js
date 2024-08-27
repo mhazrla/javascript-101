3f4485a37226cfac3ff102d685e9a3655054dbfd
mhazrla <mhazrla@gmail.com>
2024-08-27 08:49:14 +0700

    day 7 : review function

--- function.js
+++ function.js
@@ -14,9 +14,13 @@ let cetakNama = () => {}
 // printHari();
 // printHari();
 
-// function cetakHari(hari) {
-//   console.log("Hari ini adalah hari ", hari);
-// }
+function cetakHari(hari) {
+  console.log("Hari ini adalah hari ", hari);
+}
+
+// cetakHari("Selasa");
+// cetakHari("Rabu");
+// cetakHari("Ksamois");
 
 // cetakHari("Rabu");
 
@@ -25,29 +29,34 @@ let cetakNama = () => {}
 //   console.log(`Halo nama saya ${nama} Saya berumur ${umur}`);
 // }
 
-// cetakDataDiri(undefined, 14);
-
-// let x; // global scope
-
+// cetakDataDiri(undefined, 16);
+// let perkalian; // undefined
 // function hitungDuaAngka(x, y) {
-//   let result = x + y;
-//   x = result;
-//   let angka = 12; // local scope
-//   return angka;
+//   perkalian = x * y; // 2
 // }
 
+// console.log("sebelum function diujalankan", perkalian);
+// hitungDuaAngka(2, 2);
+// console.log("setelah function diujalankan", perkalian);
 // console.log(hitungDuaAngka(2, 5));
+
 // rest parameter = untuk menampung banyak value
-// function hitungBanyakAngka(...angka) {
+
+// function hitungBanyakAngka(nama, ...angka) {
 //   let result = 0;
 
 //   for (let x of angka) {
-//     console.log(x);
+//     console.log("nilai yang akan ditambahkan", x);
 //     result += x;
+//     console.log("hasil dari penjumlahan di dalam looping: ", result);
 //   }
 
 //   console.log("Hasil akhirnya adalah ", result);
+//   console.log(nama);
+//   console.log(angka);
 // }
+
+// hitungBanyakAngka("Adam", 1, 2, 3, 4, 5);
 
 // hitungBanyakAngka(1, 2, 3, 4, 5);
 
@@ -62,3 +71,16 @@ let cetakNama = () => {}
 // };
 
 // cetakNama("AB", "BC", "CD");
+
+// let printNama = function cetakNama() {
+//   console.log("Halo");
+// };
+
+// printNama()
+
+const printNama = (a, b, c) => {
+  console.log(a, b, c);
+  console.log("Halo");
+};
+
+printNama(1, 2, 3);
