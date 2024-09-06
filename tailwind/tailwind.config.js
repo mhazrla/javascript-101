435df2da9908d918ad58d4897cccf5862064b7c8
mhazrla <mhazrla@gmail.com>
2024-09-06 08:53:56 +0700

    tailwind slicing

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,19 +3,6 @@ module.exports = {
   content: ["./src/*.{html,js,css}"],
   theme: {
     extend: {},
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      purple: "#3f3cbb",
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
-      kocakbos: "red",
-    },
   },
   plugins: [],
 };
